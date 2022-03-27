62d4f31e1c63a01888c79d6aee86cfdfd01f705c
EdenYigzaw <31193308+EdenYigzaw@users.noreply.github.com>
2022-03-27 11:12:37 -0400

    Add files via upload

---
+++ FaceDetection_Main.py
@@ -0,0 +1,65 @@
+#Face Detection inspired by https://docs.opencv.org/3.3.0/d7/d8b/tutorial_py_face_detection.html
+
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import tensorflow as tf
+from tensorflow import keras
+
+#Declare cascade classifier used for Face Detection (Uses the pre-trained Haar Cascade Classifier)
+
+frontFaceClassifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+#Declare Model to be used for Emotion Recognition and Emotion Classes
+
+#emotionModel = keras.models.load_model("")
+
+#Get default webcam source
+webcamSource = cv2.VideoCapture(0)
+webcamSource.set(3,500) #Set display window Width
+webcamSource.set(4,500) #Set display window Height
+
+#Main while loop variable
+capturingVideo = True
+
+while capturingVideo:
+    
+    #Read one frame of the video
+    _, videoFrame = webcamSource.read()
+
+    #Search for face and draw a rectangle on its location
+    gray = cv2.cvtColor(videoFrame, cv2.COLOR_BGR2GRAY)
+    scaleFactor = 1.15
+    minNeighbors = 6
+    faces = frontFaceClassifier.detectMultiScale(gray, scaleFactor, minNeighbors)
+
+    for(x, y, width, height) in faces:
+        #Draw a rectangle
+        topLeftRectangle = (x,y) #Top left coordinates of expected rectangle
+        recWidth = (x + width)
+        recHeight = (y + height)
+        bottomRightRectangle = (recWidth, recHeight) #Bottom right coordinates of expected rectangle
+        color = (255,0,0)
+        colorThickness = 3
+
+        cv2.rectangle(videoFrame, topLeftRectangle, bottomRightRectangle, color, colorThickness)
+        
+
+
+
+
+    #Create and display a window showing the webcam video
+    cv2.imshow('Display Window', videoFrame)
+    cv2.waitKey(1)
+
+
+    #If user clicks on the x button in the display frame, stop video capture
+    if cv2.getWindowProperty('Display Window', cv2.WND_PROP_VISIBLE) < 1 :
+        capturingVideo = False
+
+
+#Release capture and close any associated windows
+webcamSource.release()
+cv2.destroyAllWindows()
---
+++ MaskDetection_Model.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+
+from tensorflow import keras
+from keras import layers
+from keras.preprocessing.image import image_dataset_from_directory
+
+
+
+#Step 1: Get Training and Test Data for Model from Dataset 
+
+
+
+training_dataset = image_dataset_from_directory(
+    directory = "DataSet/train",
+    image_size = (48, 48),
+    color_mode = "grayscale",
+    batch_size = 32,
+    shuffle = True,
+    class_names = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+)
+
+
+test_dataset = image_dataset_from_directory(
+    directory = "DataSet/test",
+    image_size = (48, 48),
+    color_mode = "grayscale",
+    batch_size = 32,
+    shuffle = False,
+    class_names = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+)
+
+
+
+#Step 2: CNN Model 
+
+#CNN From KERAS
+inputs = keras.Input(shape=(48, 48, 1))
+
+#Standardization -> Put image values to [0,1] instead of [0,255]
+cnn_component = layers.Rescaling(1./255)(inputs)
+
+#First Layer
+cnn_component = layers.Conv2D(filters = 64, kernel_size = 3)(inputs)
+cnn_component = layers.BatchNormalization()(cnn_component)
+cnn_component = layers.Activation("relu")(cnn_component)
+cnn_component = layers.Dropout(0.2)(cnn_component)
+cnn_component = layers.MaxPooling2D(pool_size=2)(cnn_component)
+
+
+#Second Layer
+cnn_component = layers.Conv2D(filters = 128, kernel_size = 3)(cnn_component)
+cnn_component = layers.BatchNormalization()(cnn_component)
+cnn_component = layers.Activation("relu")(cnn_component)
+cnn_component = layers.Dropout(0.2)(cnn_component)
+cnn_component = layers.MaxPooling2D(pool_size=2)(cnn_component)
+
+#Third Layer
+cnn_component = layers.Conv2D(filters = 256, kernel_size = 3)(cnn_component)
+cnn_component = layers.BatchNormalization()(cnn_component)
+cnn_component = layers.Activation("relu")(cnn_component)
+cnn_component = layers.Dropout(0.2)(cnn_component)
+cnn_component = layers.MaxPooling2D(pool_size=2)(cnn_component)
+
+
+
+#Fully Connected Layers
+cnn_component = layers.Flatten()(cnn_component)
+cnn_component = layers.Dense(256, activation = "elu")(cnn_component)
+outputs = layers.Dense(7, activation = "softmax")(cnn_component)
+
+cnn_model = keras.Model(inputs=inputs, outputs=outputs)
+
+
+#Detailed Summary on the CNN Model
+#Step 3: Compile, Train and Test Model with Accuracy as the primary evaluation metric
+cnn_model.summary()
+
+cnn_model.compile(optimizer= keras.optimizers.Adam(learning_rate = 0.0001), loss = "sparse_categorical_crossentropy", metrics = ["accuracy"])
+
+cnn_model.fit(training_dataset, epochs = 20, batch_size = 32)
+
+testing_loss, testing_accuracy = cnn_model.evaluate(test_dataset)
+print(f"test accuracy: {testing_accuracy}")
+
+
+
+
+#NOTE: CURRENT CNN: @ EPOCH 20: 87% TRAIN ACCURACY, LOSS: 0.3661, TEST ACCURACY: 0.572
+
+
+#Step 4: Save the Model with its current weight values
+cnn_model.save("fer_model_v1")
